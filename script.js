bfc54428d346e62033321e36bdba9d49870179c2
Adityazingade896 <adityazingade.896@gmail.com>
2025-03-27 20:21:46 +0530

    fole updated

--- script.js
+++ script.js
@@ -21,19 +21,19 @@ async function checkwheather(city){
         document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
         document.querySelector(".wind").innerHTML=data.wind.speed+"km/h";
         if(data.weather[0].main=="Clouds"){
-            weatherIcon.src="images/clouds.png";
+            weatherIcon.src="/IMAGES/clouds.png";
         }
         else if(data.weather[0].main=="Clear"){
-            weatherIcon.src="images/clear.png";
+            weatherIcon.src="/IMAGES/clear.png";
         }
         else if(data.weather[0].main=="Rain"){
-            weatherIcon.src="images/rain.png";
+            weatherIcon.src="/IMAGES/rain.png";
         }
         else if(data.weather[0].main=="Drizzle"){
-            weatherIcon.src="images/drizzle.png";
+            weatherIcon.src="/IMAGES/drizzle.png";
         }
         else if(data.weather[0].main=="Mist"){
-            weatherIcon.src="images/mist.png";
+            weatherIcon.src="/IMAGES/mist.png";
         }
         document.querySelector(".weather").style.display="block";
         document.querySelector(".error").style.display="none";
